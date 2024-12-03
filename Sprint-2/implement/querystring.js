12d40a5e014ec3e7dc3cb53cbdae747009d0a9e6
Sajad <sajadsoorni@gmail.com>
2024-12-03 14:59:05 +0000

    handle missing values in query string parsing
    - Updated parseQueryString to assign undefined for keys without values.
    - Fixed handling for cases with no '=' or incomplete key-value pairs.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -4,13 +4,18 @@ function parseQueryString(queryString) {
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
-
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    const [key, ...valueParts] = pair.split("=");
+    const value = valueParts.length > 0 ? valueParts.join("=") : undefined;
     queryParams[key] = value;
   }
 
   return queryParams;
 }
+// Test:
+// console.log(parseQueryString("name=Sajad&age&=35"));
+// console.log(parseQueryString("equation=x=y+1"));
+// console.log(parseQueryString("name=Sajad&name=Kevin"));
+// console.log(parseQueryString("name&age"));
 
 module.exports = parseQueryString;
