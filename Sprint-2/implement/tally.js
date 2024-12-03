f3e89cc980b7634b893713b52b9e9cee101f9923
Sajad <sajadsoorni@gmail.com>
2024-12-03 17:41:44 +0000

    implement tally function to count occurrences of unique items in an array
    - The function takes an array of items as input.
    - Returns an object with the count of each unique item.
    - Handles invalid input by throwing an error if the input is not an array.
    - Handles an empty array by returning an empty object.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
+function tally(listOfItems) {
+  if (!Array.isArray(listOfItems)) {
+    throw new Error("Invalid input: expected an array");
+  }
+  const obj = {};
+  for (const element of listOfItems) {
+    obj[element] = (obj[element] || 0) + 1;
+  }
+
+  return obj;
+}
 
 module.exports = tally;
