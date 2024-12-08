3e04343dab151abd1c61d1d8ac3cfdbad80c9710
Sajad <sajadsoorni@gmail.com>
2024-12-08 16:16:01 +0000

    Implement alarm countdown functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,58 @@
-function setAlarm() {}
+//Global Variables
+const timeDisplay = document.getElementById("timeRemaining"); // Timer display element
+const alarmInput = document.getElementById("alarmSet"); // Input field for alarm time
+let countDownInterval; // To store interval ID for the countdown
+
+// Change display text color to red
+
+function redColor() {
+  timeDisplay.style.color = "red";
+}
+// Reset display text color to black
+
+function blackColor() {
+  timeDisplay.style.color = "black";
+}
+// Clear the input field
+
+function clearInputBox() {
+  alarmInput.value = "";
+}
+
+//Main Function
+function setAlarm() {
+  //clear any existing interval if one is running
+
+  if (countDownInterval) {
+    clearInterval(countDownInterval);
+  }
+
+  //get the input value and convert it to a number
+
+  let time = Number(alarmInput.value);
+  if (!isNaN(time) && time > 0) {
+    const updateDisplay = () => {
+      const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+      const seconds = String(time % 60).padStart(2, "0");
+      timeDisplay.textContent = `Time Remaining: ${minutes}:${seconds}`;
+    };
+    updateDisplay();
+    //Start the countdown
+
+    countDownInterval = setInterval(() => {
+      if (time <= 0) {
+        clearInterval(countDownInterval);
+        redColor();
+        playAlarm();
+      } else {
+        time -= 1;
+        updateDisplay();
+      }
+    }, 1000); //run every 1000ms(1 Second)
+  } else {
+    time.timeDisplay.textContent = `Please enter a valid number!`;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -10,6 +64,8 @@ function setup() {
   });
 
   document.getElementById("stop").addEventListener("click", () => {
+    clearInputBox();
+    blackColor();
     pauseAlarm();
   });
 }
