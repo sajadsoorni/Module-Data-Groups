0d402b9b273b820d981dbe1c8a66dcc093084be0
Sajad <sajadsoorni@gmail.com>
2024-12-19 13:06:04 +0000

    Fix: Handle NaN values in findMax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,7 +1,7 @@
 function findMax(elements) {
   let max = -Infinity;
   for (i = 0; i < elements.length; i++) {
-    if (typeof elements[i] === "number") {
+    if (typeof elements[i] === "number" && !isNaN(elements[i])) {
       if (elements[i] > max) {
         max = elements[i];
       }
