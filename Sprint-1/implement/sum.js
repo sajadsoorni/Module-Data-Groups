676a04d0601ed0e50b78ca5ebec98f85212924e0
Sajad <sajadsoorni@gmail.com>
2024-12-19 13:32:56 +0000

    Ignore NaN values in the sum calculation, while keeping Infinity handling intact.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,7 +1,7 @@
 function sum(elements) {
   sum = 0;
   for (let i = 0; i < elements.length; i++) {
-    if (typeof elements[i] === "number") {
+    if (typeof elements[i] === "number" && !isNaN(elements[i])) {
       sum = elements[i] + sum;
     }
   }
