4deb2b1b6ada909b3de5fc287e2d728995ce748e
Sajad <sajadsoorni@gmail.com>
2024-12-19 12:35:19 +0000

    Fix: Clone array before sorting and remove debugging code

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,10 +3,10 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  list.sort((a, b) => a - b);
-  console.log([list].join(""));
-  const middleIndex = Math.floor(list.length / 2);
-  if (list.length % 2 === 0) {
+  const sortedList = [...list]; //clone the array
+  sortedList.sort((a, b) => a - b); //sort the cloned array
+  const middleIndex = Math.floor(sortedList.length / 2);
+  if (sortedList.length % 2 === 0) {
     return (list[middleIndex - 1] + list[middleIndex]) / 2;
   } else {
     return list[middleIndex];
